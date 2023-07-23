53e02296f7c253188b913b0f963608d02330cbf6
kumarmudur <kumarmudur10@gmail.com>
2023-07-22 20:06:10 -0400

    [ReactJS] basic  example

--- App.js
+++ App.js
@@ -1,5 +1,28 @@
-const heading = React.createElement('h1', {id: 'heading'}, 'Hello World from React');
+// const heading = React.createElement('h1', {id: 'heading'}, 'Hello World from React');
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// root.render(heading); 
+
+{/* <div id="parent">
+    <div id="child">
+        <h1>I'm h1 tag!</h1>
+    </div>
+</div> */}
+
+const parent = React.createElement(
+    "div",
+    { id: 'parent'},
+    React.createElement(
+        "div",
+        { id: 'child' },
+        [
+            React.createElement("h1", {}, "I'm h1 tag!"),
+            React.createElement("h1", {}, "I'm h2 tag!")
+        ]
+    )
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(heading); 
+root.render(parent); 
