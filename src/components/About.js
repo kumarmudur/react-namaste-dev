8d4d06127357716c33f8e34345fdcc51e1a2c844
kumarmudur <kumarmudur10@gmail.com>
2023-08-14 16:44:51 -0400

    [ReactJS] about component changes

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import User from './User';
+import UserClass from './UserClass';
 const About = () => {
     return (
         <div>
             <h1>About</h1>
             <h2>This is Namaste React Web Series</h2>
+            {/*<User name={"Shiva (Function)"} location={"New York (Function)"}/>*/}
+            <UserClass name={"Shiva (Class)"} location={"New York (Class)"}/>
         </div>
     );
 };
