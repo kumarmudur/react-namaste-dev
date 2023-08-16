3a6e4f37179b6af5f2a0e6f16ccaf5ceed2af37b
kumarmudur <kumarmudur10@gmail.com>
2023-08-15 20:41:13 -0400

    [ReactJS] Header component changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React, {useEffect} from "react";
+import { Link } from "react-router-dom";
 
 import { LOGO_URL } from '../utils/constants';
 const Header = () => {
@@ -20,8 +21,12 @@ const Header = () => {
             <div className="nav-items">
                 <ul>
                     <li>Home</li>
-                    <li>About Us</li>
-                    <li>Contact Us</li>
+                    <li>
+                        <Link to="/about">About Us</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact Us</Link>
+                    </li>
                     <li>Cart</li>
                     <button className="login">Login</button>
                 </ul>
